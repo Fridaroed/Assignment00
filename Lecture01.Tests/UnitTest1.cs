ffb39ad749c8dd383c4d4fb359f1a1e00dfaf441
frir <frir@itu.dk>
2021-09-08 14:36:06 +0200

    Implemented error handling

--- Lecture01.Tests/UnitTest1.cs
+++ Lecture01.Tests/UnitTest1.cs
@@ -1,14 +1,59 @@
 using System;
+using System.IO;
 using Xunit;
+using Lecture01;
 
 namespace Lecture01.Tests
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void main_isLeapYearTrue()
         {
+
+            var output = false;
+
+            output = Program.isLeapYear(2000);
+
+            Assert.True(output);                
+        }
+
+        [Fact]
+        public void print_nay()
+        {
+            //Arrange
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+
+            var input = new StringReader("2001");
+            Console.SetIn(input);
+
+            //Act
+            Program.Main(new string[0]);         
+
+            var output = writer.GetStringBuilder().ToString().Trim();
+            Assert.Equal("nay", output);
+
+        }
+
+        [Fact]
+        public void print_exception()
+        {
+            //Arrange
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+
+            var input = new StringReader("1234");
+            Console.SetIn(input);
+
+            //Act
+            Program.Main(new string[0]);         
+
+            var output = writer.GetStringBuilder().ToString().Trim();
+            Assert.Equal("Year must be from 1582 or later", output);
 
         }
     }
 }
+
+
--- Lecture01/Program.cs
+++ Lecture01/Program.cs
@@ -2,11 +2,72 @@
 
 namespace Lecture01
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private bool isLeapYearBool;
+            public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            userInput_IsLeapYear(Convert.ToInt32(Console.ReadLine()));
         }
+
+
+        public static bool isLeapYear(int year){
+
+            if (year % 4 == 0)
+            {
+                if ((year % 100 != 0) || (year % 400 == 0))
+                {
+                    return true;
+                } else
+                {
+                    return false;
+                }
+                
+            }
+            return false;
+
+        }
+
+
+        public static void userInput_IsLeapYear(int year) {
+
+            try {
+
+            if (year < 1582)
+            {
+                throw new ArgumentException("Year must be from 1582 or later");
+            } 
+
+            if (isLeapYear(year)) 
+            {
+                Console.WriteLine("yay");
+            } else
+            {
+                Console.WriteLine("nay");
+            }
+            } catch (ArgumentException e){
+                Console.WriteLine(e.Message);
+            }
+
+
+        }
+
+        
     }
 }
+
+
+
+/* LeapYear LY = new LeapYear();
+            try {
+                int year = int.Parse(Console.ReadLine());
+                LY.isLeapYear(year);
+            }
+            catch (ArgumentException e) {
+                Console.WriteLine(e.Message);
+            }
+            catch (FormatException) {
+                Console.WriteLine("Please type in year");
+            }
+ */
+
